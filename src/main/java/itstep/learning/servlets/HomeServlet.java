57fca8e473d0860cff225ae5deb6963d4b884595
Alex Yelienok <aelenok@gmail.com>
2025-01-24 16:45:40 +0100

    робота на уроці + ДЗ 22.01.2025

---
+++ src/main/java/itstep/learning/servlets/HomeServlet.java
@@ -0,0 +1,30 @@
+package itstep.learning.servlets;
+
+import com.google.gson.Gson;
+import itstep.learning.rest.RestResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet("/home")
+
+public class HomeServlet extends HttpServlet {
+    private final Gson gson = new Gson();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.getWriter().print(
+                gson.toJson(
+                        new RestResponse()
+                .setStatus(200)
+                .setMessage("Ok")
+                )
+        );
+    }
+}
---
+++ src/main/java/itstep/learning/servlets/TimeServlet.java
@@ -0,0 +1,35 @@
+package itstep.learning.servlets;
+
+import com.google.gson.Gson;
+import itstep.learning.rest.RestResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+@WebServlet("/time")
+
+public class TimeServlet extends HttpServlet {
+    private final Gson gson = new Gson();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long timestamp = System.currentTimeMillis();
+
+        String isoTime = Instant.ofEpochMilli(timestamp)
+                .atZone(ZoneId.systemDefault())
+                .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
+        resp.getWriter().print(
+                gson.toJson(
+                        new RestResponse()
+                                .setStatus(200)
+                                .setMessage("Timestamp: " + timestamp + ", ISO: " + isoTime)
+                )
+        );
+    }
+}
