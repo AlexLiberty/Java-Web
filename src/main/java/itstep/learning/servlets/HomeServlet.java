24eabf3ae3079e085a6368f2fc743d14896342a5
Alex Yelienok <aelenok@gmail.com>
2025-01-26 10:33:21 +0100

    ДЗ 24.01.2025

--- src/main/java/itstep/learning/servlets/HomeServlet.java
+++ src/main/java/itstep/learning/servlets/HomeServlet.java
@@ -9,6 +9,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @WebServlet("/home")
@@ -18,12 +21,63 @@ public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String message;
+        //підключення до бази даних
+        try {
+            DriverManager.registerDriver(
+                    new com.mysql.cj.jdbc.Driver()
+            );
+            String connectionString = "jdbc:mysql://localhost:3308/javaDb";
+            Connection connection = DriverManager.getConnection(
+                    connectionString,
+                    "user1",
+                    "pass123"
+            );
+
+            String sql = "SELECT CURRENT_TIMESTAMP"; //запит
+            Statement statement = connection.createStatement(); // інструмент передачі запиту у базу даних
+            ResultSet resultSet = statement.executeQuery(sql); // виконання запиту, використовуючи різні інструменти
+            resultSet.next(); //
+
+            message = resultSet.getString(1);// !!! JDBC відлік з 1
+
+        } catch (SQLException e) {
+            message = e.getMessage();
+        }
+
+        // Підключення до бази даних
+        try {
+            DriverManager.registerDriver(
+                    new com.mysql.cj.jdbc.Driver()
+            );
+            String connectionString = "jdbc:mysql://localhost:3308/javaDb";
+            Connection connection = DriverManager.getConnection(
+                    connectionString,
+                    "user1",
+                    "pass123"
+            );
+
+            String sql = "SHOW DATABASES"; // Запит на отримання списку баз даних
+            Statement statement = connection.createStatement(); // Інструмент для виконання запиту
+            ResultSet resultSet = statement.executeQuery(sql); // Виконання запиту
+
+            List<String> databases = new ArrayList<>();
+            while (resultSet.next()) {
+                databases.add(resultSet.getString(1));
+            }
+
+            message = String.join(", ", databases);
+
+        } catch (SQLException e) {
+            message = e.getMessage();
+        }
+
         resp.setContentType("application/json");
         resp.getWriter().print(
                 gson.toJson(
                         new RestResponse()
                 .setStatus(200)
-                .setMessage("Ok")
+                .setMessage(message)
                 )
         );
     }
