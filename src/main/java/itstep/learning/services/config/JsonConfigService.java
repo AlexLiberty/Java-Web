f3223db44eed253f3b56b2d9dbd6de9662e45d63
Alex Yelienok <aelenok@gmail.com>
2025-03-11 11:25:45 +0100

    Дз  03.03.2025

--- src/main/java/itstep/learning/services/config/JsonConfigService.java
+++ src/main/java/itstep/learning/services/config/JsonConfigService.java
@@ -13,8 +13,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Singleton
-public class JsonConfigService implements ConfigService
-{
+public class JsonConfigService implements ConfigService {
     private JsonElement configElement;
     private final Logger logger;
 
@@ -24,41 +23,29 @@ public class JsonConfigService implements ConfigService
     }
 
     @Override
-    public JsonPrimitive getValue(String path)
-    {
-        if(configElement == null)
-        {
+    public JsonPrimitive getValue(String path) {
+        if (configElement == null) {
             init();
         }
 
         String[] parts = path.split("\\.");
         JsonObject obj = configElement.getAsJsonObject();
 
-        for (int i =0; i<parts.length - 1; ++i)
-        {
+        for (int i = 0; i < parts.length - 1; ++i) {
             JsonElement je = obj.get(parts[i]);
-            if(je == null)
-            {
-                throw new NoSuchElementException(
-                        "Part of path '" +parts[i]+ "' not found");
+            if (je == null) {
+                throw new NoSuchElementException("Part of path '" + parts[i] + "' not found");
             }
-
-           obj = je.getAsJsonObject();
+            obj = je.getAsJsonObject();
         }
         return obj.getAsJsonPrimitive(parts[parts.length - 1]);
     }
 
-    private void init()
-    {
-        try(Reader reader = new InputStreamReader(
-                this.getClass()
-                        .getClassLoader()
-                .getResourceAsStream ("appsettings.json")))
-        {
+    private void init() {
+        try (Reader reader = new InputStreamReader(
+                this.getClass().getClassLoader().getResourceAsStream("appsettings.json"))) {
             configElement = new Gson().fromJson(reader, JsonElement.class);
-        }
-        catch (IOException ex)
-        {
+        } catch (IOException ex) {
             logger.log(Level.SEVERE, "JsonConfigService::init", ex);
         }
     }
--- src/main/java/itstep/learning/services/storage/DiskStorageService.java
+++ src/main/java/itstep/learning/services/storage/DiskStorageService.java
@@ -1,33 +1,37 @@
 package itstep.learning.services.storage;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import itstep.learning.services.config.ConfigService;
 
 import java.io.*;
 import java.util.UUID;
 
 @Singleton
-public class DiskStorageService implements StorageService
-{
-    private final String storagePath = "D:/Java/storage/";
+public class DiskStorageService implements StorageService {
+    private final String storagePath;
+
+    @Inject
+    public DiskStorageService(ConfigService configService) {
+        this.storagePath = configService.getValue("storage.path").getAsString();
+    }
+
     @Override
-    public String put(InputStream inputStream, String ext) throws IOException
-    {
+    public String put(InputStream inputStream, String ext) throws IOException {
         String itemId = UUID.randomUUID() + ext;
         File file = new File(storagePath + itemId);
-        FileOutputStream writer = new FileOutputStream(file);
-        byte[] buf = new byte[131072];
-        int len;
-        while ((len=inputStream.read(buf))>0)
-        {
-            writer.write(buf, 0, len);
+        try (FileOutputStream writer = new FileOutputStream(file)) {
+            byte[] buf = new byte[131072];
+            int len;
+            while ((len = inputStream.read(buf)) > 0) {
+                writer.write(buf, 0, len);
+            }
         }
-        writer.close();
         return itemId;
     }
 
     @Override
-    public InputStream get(String itemId) throws IOException
-    {
+    public InputStream get(String itemId) throws IOException {
         return new FileInputStream(storagePath + itemId);
     }
 }
